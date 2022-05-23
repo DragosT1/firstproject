10c1c943cbe190e06822bbdbee483d851d62fb35
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-23 19:12:21 +0300

    sortlist

--- src/myproject/main.py
+++ src/myproject/main.py
@@ -1,6 +1,6 @@
 from myproject.parserart.parseArticle import *
 from myproject.Fibonacci.fibonacci import *
-
+from myproject.sortlist.sortlist import *
 
 
 if __name__ == "__main__":
@@ -9,3 +9,8 @@ if __name__ == "__main__":
 
     nterms = int(input("How many terms? "))
     fibonacci(nterms)
+
+    list1 = [1, 5, 6, 9, 11]
+    list2 = [3, 4, 7, 8, 10]
+
+    sortlist(list1, list2)
--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,16 +1,27 @@
 from myproject.parserart.parseArticle import *
 from myproject.Fibonacci.fibonacci import *
 from myproject.divisors.divisors import *
+from myproject.sortlist.sortlist import *
+
 
 def test_parseArticle():
-    listinput = ['iceland', 'economy', 'weathered']
+    listinput = ["iceland", "economy", "weathered"]
     c = countTotalWords(listinput)
     assert c == 3
+
 
 def test_fibonnaci():
     var = fibonacci(70)
     assert var == 190392490709135
 
+
 def test_divizori():
     var = divisors(24)
     assert var == [1, 2, 3, 4, 6, 8, 12, 24]
+
+
+def test_sortlist():
+    list1 = [1, 5, 6, 9, 11]
+    list2 = [3, 4, 7, 8, 10]
+    var = sortlist(list1, list2)
+    assert var == [1, 3, 4, 5, 6, 7, 8, 9, 10, 11]
