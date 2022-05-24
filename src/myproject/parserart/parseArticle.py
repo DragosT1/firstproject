0a13e3c49f5e226d2451af4531fe6d9380698e31
DragosT1 <98753117+DragosT1@users.noreply.github.com>
2022-05-24 12:46:15 +0300

    refactoring

--- src/myproject/parserart/parseArticle.py
+++ src/myproject/parserart/parseArticle.py
@@ -10,81 +10,81 @@
 # remove all the punctuation
 # don’t count words with less than 3 characters
 
+
 def replaceUnwantedChar(inputText):
-    text = inputText.readlines()
-    onestring = ""
+    onestring = inputText.read().lower()
+
     fullWordList = []
-    char_to_replace = {'.': '',
-                       ',': '',
-                       '!': '',
-                       '?': '',
-                       '/': '',
-                       '\n': '',
-                       '-': '',
-                       '_': '',
-                       '’': ' ',
-}
-    # Build one string form all lines and make lowercase everything
-    for i in range(0, len(text)):
-        onestring = onestring + str(text[i]).lower()
+    char_to_replace = {
+        ".": "",
+        ",": "",
+        "!": "",
+        "?": "",
+        "/": "",
+        "\n": "",
+        "-": "",
+        "_": "",
+        "’": " ",
+    }
 
     # Iterate over all key-value pairs in dictionary to remove unwanted signs
     for key, value in char_to_replace.items():
         # Replace key character with value character in string
         onestring = onestring.replace(key, value)
-    
-    #Iteration to remove prepositions
+
+    # Iteration to remove prepositions
     for word in onestring.split(" "):
         if len(word) <= 3:
             word = ""
-        else: fullWordList.append(word)
-    #     print(word)
-    # print(fullWordList)
-    # print(onestring)
+        else:
+            fullWordList.append(word)
+
     return fullWordList
+
 
 def findPhoneNb(listinput):
     list = []
     for word in listinput:
-        if word[0] == "0" and len(word) == 10:
+        if word[0] == "0" and word[1] == "7" and word[2] != "0" and len(word) == 10:
             list.append(word)
-    print("Phone numbers: "+str(list))
+    print("Phone numbers: " + str(list))
+
 
 def countTotalWords(listinput2):
     print("Total number of words: " + str(len(listinput2)))
-    return(len(listinput2))
+    return len(listinput2)
 
-def countUniqueWords(listinput3):
+
+def countUniqueWordsTOP5(listinput3):
     # initialize a null list
     unique_list = []
+    dicts = {}
 
     # traverse for all elements
     for word in listinput3:
         # check if exists in unique_list or not
         if word not in unique_list:
             unique_list.append(word)
+
+    # Build dict from known unique words as keys and count key appearances
+    for element in unique_list:
+        dicts[element] = 0
+        for allword in listinput3:
+            if allword == element:
+                dicts[element] += 1
+    # Sort dict and print top 5 key appearances
+    sortedunuque = sorted(dicts.items(), key=lambda val: val[1], reverse=True)
+    print(sortedunuque)
+    for topWords in range(5):
+        print("top" + str(topWords + 1) + " word appearance is: " + str(sortedunuque[topWords]))
+
     return unique_list
 
-def top5Words (list1,list2):
-    dicts = {}
-    for wordUnique in range(len(list2)):
-        dicts[list2[wordUnique]] = 0
-        for allWords in range(len(list1)):
-            if list1[allWords] == list2[wordUnique]:
-                dicts[list2[wordUnique]] += 1
-
-    sortedunuque = (sorted(dicts.items(), key=lambda val: val[1]))
-    print("top1 word appearance is: " + str(sortedunuque[len(sortedunuque)-1]))
-    print("top2 word appearance is: " + str(sortedunuque[len(sortedunuque) - 2]))
-    print("top3 word appearance is: " + str(sortedunuque[len(sortedunuque) - 3]))
-    print("top4 word appearance is: " + str(sortedunuque[len(sortedunuque) - 4]))
-    print("top5 word appearance is: " + str(sortedunuque[len(sortedunuque) - 5]))
 
 def parseArticleSuite(path):
     input = open(path, "r")
     cleanedText = replaceUnwantedChar(input)
     findPhoneNb(cleanedText)
     countTotalWords(cleanedText)
-    uniqueWordsList = countUniqueWords(cleanedText)
+    uniqueWordsList = countUniqueWordsTOP5(cleanedText)
     print("Number of unique words is: " + str(len(uniqueWordsList)))
-    top5Words(cleanedText, uniqueWordsList)
